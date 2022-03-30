4663dd0efa9a9b001ce9713eccc3455d9d67b17e
AsterixxxGallier <asterixxxgallier@aol.com>
2022-03-30 15:03:54 +0200

    Add Position struct

--- src/spaced_lists/traversal/mod.rs
+++ src/spaced_lists/traversal/mod.rs
@@ -1,3 +1,14 @@
+use crate::{Spacing, SpacedList};
+
 pub(crate) mod node;
 
 pub(crate) mod range;
+
+pub struct Position<'a, S: Spacing, List: SpacedList<S>> {
+    lists: Vec<&'a List>,
+    pub index: usize,
+    pub position: S,
+    link_index: usize,
+    offset: usize,
+    mask: usize,
+}
