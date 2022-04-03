58d2d09438aa276c5669137a13a86d95f0af07bf
AsterixxxGallier <asterixxxgallier@aol.com>
2022-04-03 17:28:11 +0200

    Complete first integration test

--- tests/hollow_spaced_list.rs
+++ tests/hollow_spaced_list.rs
@@ -9,18 +9,139 @@ fn test() {
     list.insert_node(5);
     list.insert_node(3);
 
-    let pos = list.node_before(-1);
+    let query_pos = -1;
+
+    let pos = list.node_before(query_pos);
     assert!(pos.is_none());
 
-    let pos = list.node_at_or_before(-1);
+    let pos = list.node_at_or_before(query_pos);
     assert!(pos.is_none());
 
-    let pos = list.node_at(-1);
+    let pos = list.node_at(query_pos);
     assert!(pos.is_none());
 
-    let pos = list.node_at_or_after(-1).unwrap();
+    let pos = list.node_at_or_after(query_pos).unwrap();
     assert_eq!(pos.position(), 0);
 
-    let pos = list.node_after(-1).unwrap();
+    let pos = list.node_after(query_pos).unwrap();
     assert_eq!(pos.position(), 0);
+
+    let query_pos = 0;
+
+    let pos = list.node_before(query_pos);
+    assert!(pos.is_none());
+
+    let pos = list.node_at_or_before(query_pos).unwrap();
+    assert_eq!(pos.position(), 0);
+
+    let pos = list.node_at(query_pos).unwrap();
+    assert_eq!(pos.position(), 0);
+
+    let pos = list.node_at_or_after(query_pos).unwrap();
+    assert_eq!(pos.position(), 0);
+
+    let pos = list.node_after(query_pos).unwrap();
+    assert_eq!(pos.position(), 1);
+
+    let query_pos = 1;
+
+    let pos = list.node_before(query_pos).unwrap();
+    assert_eq!(pos.position(), 0);
+
+    let pos = list.node_at_or_before(query_pos).unwrap();
+    assert_eq!(pos.position(), 1);
+
+    let pos = list.node_at(query_pos).unwrap();
+    assert_eq!(pos.position(), 1);
+
+    let pos = list.node_at_or_after(query_pos).unwrap();
+    assert_eq!(pos.position(), 1);
+
+    let pos = list.node_after(query_pos).unwrap();
+    assert_eq!(pos.position(), 3);
+
+    let query_pos = 2;
+
+    let pos = list.node_before(query_pos).unwrap();
+    assert_eq!(pos.position(), 1);
+
+    let pos = list.node_at_or_before(query_pos).unwrap();
+    assert_eq!(pos.position(), 1);
+
+    let pos = list.node_at(query_pos);
+    assert!(pos.is_none());
+
+    let pos = list.node_at_or_after(query_pos).unwrap();
+    assert_eq!(pos.position(), 3);
+
+    let pos = list.node_after(query_pos).unwrap();
+    assert_eq!(pos.position(), 3);
+
+    let query_pos = 3;
+
+    let pos = list.node_before(query_pos).unwrap();
+    assert_eq!(pos.position(), 1);
+
+    let pos = list.node_at_or_before(query_pos).unwrap();
+    assert_eq!(pos.position(), 3);
+
+    let pos = list.node_at(query_pos).unwrap();
+    assert_eq!(pos.position(), 3);
+
+    let pos = list.node_at_or_after(query_pos).unwrap();
+    assert_eq!(pos.position(), 3);
+
+    let pos = list.node_after(query_pos).unwrap();
+    assert_eq!(pos.position(), 5);
+
+    let query_pos = 4;
+
+    let pos = list.node_before(query_pos).unwrap();
+    assert_eq!(pos.position(), 3);
+
+    let pos = list.node_at_or_before(query_pos).unwrap();
+    assert_eq!(pos.position(), 3);
+
+    let pos = list.node_at(query_pos);
+    assert!(pos.is_none());
+
+    let pos = list.node_at_or_after(query_pos).unwrap();
+    assert_eq!(pos.position(), 5);
+
+    let pos = list.node_after(query_pos).unwrap();
+    assert_eq!(pos.position(), 5);
+
+    let query_pos = 5;
+
+    let pos = list.node_before(query_pos).unwrap();
+    assert_eq!(pos.position(), 3);
+
+    let pos = list.node_at_or_before(query_pos).unwrap();
+    assert_eq!(pos.position(), 5);
+
+    let pos = list.node_at(query_pos).unwrap();
+    assert_eq!(pos.position(), 5);
+
+    let pos = list.node_at_or_after(query_pos).unwrap();
+    assert_eq!(pos.position(), 5);
+
+    let pos = list.node_after(query_pos);
+    assert!(pos.is_none());
+
+    let query_pos = 6;
+
+    let pos = list.node_before(query_pos).unwrap();
+    assert_eq!(pos.position(), 5);
+
+    let pos = list.node_at_or_before(query_pos).unwrap();
+    assert_eq!(pos.position(), 5);
+
+    let pos = list.node_at(query_pos);
+    assert!(pos.is_none());
+
+    let pos = list.node_at_or_after(query_pos);
+    assert!(pos.is_none());
+
+    let pos = list.node_after(query_pos);
+    assert!(pos.is_none());
 }
