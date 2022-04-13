bbdf32e70d4261638bb455ee34f100f856f7e26b
AsterixxxGallier <asterixxxgallier@aol.com>
2022-04-13 23:26:03 +0200

    Benchmark some more functions

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -12,39 +12,96 @@ fn old_(n: u32) -> u32 {
 }
 
 fn old(n: u32) -> u32 {
+    let mut counter = 0;
     let mut link_index = n;
     let mut degree = 0;
     while degree < 16 {
         let bit = 1 << degree;
-        if link_index & bit == 0 {
+        if n & bit == 0 {
+            counter += link_index;
             link_index += bit;
         }
         degree += 1;
     }
-    link_index
+    counter
 }
 
-fn new(n: u32) -> u32 {
+fn trailing_ones(n: u32) -> u32 {
+    let mut counter = 0;
     let mut link_index = n;
     let mut degree = 0;
     while degree < 16 {
         degree += (link_index >> degree).trailing_ones();
+        counter += link_index;
         link_index += 1 << degree;
         degree += 1;
     }
-    link_index
+    counter
+}
+
+const fn link_index(node_index: u32, degree: u32) -> u32 {
+    node_index + (1 << degree) - 1
+}
+
+fn node_index(n: u32) -> u32 {
+    let mut counter = 0;
+    let mut node_index = n;
+    for degree in 0..16 {
+        if n & (1 << degree) == 0 {
+            counter += link_index(node_index << degree, degree);
+        }
+        node_index >>= 1;
+    }
+    counter
+}
+
+fn node_index_opt(n: u32) -> u32 {
+    let mut counter = 0;
+    for degree in 0..16 {
+        if n & (1 << degree) == 0 {
+            counter += link_index(n >> degree << degree, degree);
+        }
+    }
+    counter
+}
+
+fn node_index_trailing_ones(n: u32) -> u32 {
+    let mut counter = 0;
+    let mut node_index = n;
+    let mut degree = 0;
+    while degree < 16 {
+        degree += node_index.trailing_ones();
+        counter += link_index(node_index << degree, degree);
+        node_index >>= 1;
+        degree += 1;
+    }
+    counter
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut counter: u16 = 0;
-    c.bench_function("old", |b| b.iter(|| {
-        old(black_box(counter as u32));
+    let mut group = c.benchmark_group("links_above");
+    group.bench_function("old", |b| b.iter(|| {
+        black_box(old(black_box(counter as u32)));
         counter += 1;
     }));
-    c.bench_function("new", |b| b.iter(|| {
-        new(black_box(counter as u32));
+    group.bench_function("trailing_ones", |b| b.iter(|| {
+        black_box(trailing_ones(black_box(counter as u32)));
         counter += 1;
     }));
+    group.bench_function("node_index", |b| b.iter(|| {
+        black_box(node_index(black_box(counter as u32)));
+        counter += 1;
+    }));
+    group.bench_function("node_index_opt", |b| b.iter(|| {
+        black_box(node_index_opt(black_box(counter as u32)));
+        counter += 1;
+    }));
+    group.bench_function("node_index_trailing_ones", |b| b.iter(|| {
+        black_box(node_index_trailing_ones(black_box(counter as u32)));
+        counter += 1;
+    }));
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
