0003fc2f75665e8458fc41158e11ef96aface87f
AsterixxxGallier <asterixxxgallier@aol.com>
2022-04-17 20:40:41 +0200

    Completely rewrite Iter, fixing it

--- src/iteration/node.rs
+++ src/iteration/node.rs
@@ -1,123 +1,21 @@
-use num_traits::zero;
-
 use crate::{Position, SpacedList, Spacing};
-use crate::traversal::link_index;
-
-struct IterPos<'list, S: 'list + Spacing, List: SpacedList<S>> {
-    list: &'list List,
-    position: S,
-    node_index: usize,
-    degree: usize,
-}
-
-impl<'list, S: 'list + Spacing, List: SpacedList<S>> Clone for IterPos<'list, S, List> {
-    fn clone(&self) -> Self {
-        Self {
-            list: self.list,
-            position: self.position,
-            node_index: self.node_index,
-            degree: self.degree,
-        }
-    }
-}
-
-impl<'list, S: 'list + Spacing, List: SpacedList<S>> Copy for IterPos<'list, S, List> {}
 
 pub struct Iter<'list, S: 'list + Spacing, List: SpacedList<S>> {
-    positions: Vec<IterPos<'list, S, List>>,
+    finished: bool,
     super_lists: Vec<&'list List>,
+    list: &'list List,
+    index: usize,
+    position: S,
 }
 
 impl<'list, S: 'list + Spacing, List: SpacedList<S>> Iter<'list, S, List> {
-    pub fn new(list: &'list List) -> Iter<'list, S, List> {
-        let mut this = Iter {
-            positions: Vec::with_capacity(list.depth()),
+    pub fn new(list: &'list List) -> Self {
+        Self {
+            finished: false,
             super_lists: vec![],
-        };
-
-        this.positions.push(IterPos {
             list,
+            index: 0,
             position: list.offset(),
-            node_index: 0,
-            degree: list.depth().saturating_sub(1),
-        });
-
-        this.descend();
-
-        this
-    }
-
-    fn position(&self) -> Option<Position<'list, S, List>> {
-        let last = self.positions.last()?;
-        Some(Position::new(self.super_lists.clone(), last.list, last.node_index, last.position))
-    }
-
-    fn next(&mut self) -> Result<(), ()> {
-        let last = self.positions.last().unwrap();
-        if let Some(sublist) = last.list.sublist_at(last.node_index) {
-            self.super_lists.push(last.list);
-            let next_position = last.position + sublist.offset();
-            self.positions.push(IterPos {
-                list: sublist,
-                position: next_position,
-                node_index: 0,
-                degree: 0,
-            });
-            return Ok(());
-        }
-
-        loop {
-            let last = self.positions.last().unwrap();
-            if last.node_index < last.list.link_size() {
-                break;
-            }
-            let mut len = self.positions.len() - last.list.depth();
-            if last.list.link_size() == 0 {
-                len -= 1;
-            }
-            self.positions.truncate(len);
-            self.super_lists.pop();
-            if len == 0 {
-                return Err(());
-            }
-        }
-
-        let last = self.positions.last().unwrap();
-
-        let len = self.positions.len() - last.node_index.trailing_ones() as usize;
-        self.positions.truncate(len);
-
-        let last = self.positions.last_mut().unwrap();
-        last.position += last.list.link_length_at(link_index(last.node_index, last.degree));
-        last.node_index += 1 << last.degree;
-
-        self.descend();
-
-        Ok(())
-    }
-
-    fn descend(&mut self) {
-        loop {
-            let last = *self.positions.last().unwrap();
-            for degree in (0..last.degree).rev() {
-                self.positions.push(IterPos {
-                    degree,
-                    ..last
-                })
-            }
-            if let Some(sublist) = last.list.sublist_at(last.node_index) {
-                if sublist.offset() == zero() {
-                    self.super_lists.push(last.list);
-                    self.positions.push(IterPos {
-                        list: sublist,
-                        position: last.position,
-                        node_index: 0,
-                        degree: sublist.depth().saturating_sub(1),
-                    });
-                    continue;
-                }
-            }
-            break;
         }
     }
 }
@@ -126,9 +24,41 @@ impl<'list, S: 'list + Spacing, List: SpacedList<S>> Iterator for Iter<'list, S,
     type Item = Position<'list, S, List>;
 
     fn next<'a>(&'a mut self) -> Option<Position<'list, S, List>> {
-        let position = Iter::<'_, _, _>::position(self)?;
-        // if we're at the end of the list, the line above will return None in the next iteration
-        let _err_if_at_end = self.next();
-        Some(position)
+        if self.finished {
+            return None
+        }
+
+        let old_position =
+            Position::new(self.super_lists.clone(), self.list, self.index, self.position);
+
+        if self.index == self.list.link_size() {
+            match self.super_lists.pop() {
+                Some(super_list) => {
+                    let index = self.list.index_in_super_list().unwrap();
+                    self.position -= self.list.last_position();
+                    self.list = super_list;
+                    self.position += self.list.link_length_at_node(index);
+                    self.index = index + 1;
+                }
+                None => {
+                    self.finished = true;
+                }
+            }
+        } else {
+            match self.list.sublist_at(self.index) {
+                Some(sublist) => {
+                    self.super_lists.push(self.list);
+                    self.list = sublist;
+                    self.index = 0;
+                    self.position += sublist.offset();
+                }
+                None => {
+                    self.position += self.list.link_length_at_node(self.index);
+                    self.index += 1;
+                }
+            }
+        }
+
+        Some(old_position)
     }
 }
