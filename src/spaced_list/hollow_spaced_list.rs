b48584ef3634adb603403ac7ebfce80477ae8295
AsterixxxGallier <asterixxxgallier@aol.com>
2022-04-18 23:13:26 +0200

    Make the HollowSpacedList delegate of iter not take a mutable reference to self

--- src/spaced_list/hollow_spaced_list.rs
+++ src/spaced_list/hollow_spaced_list.rs
@@ -9,7 +9,7 @@ impl<S: Spacing> HollowSpacedList<S> {
     delegates! {
         as CrateSpacedList<S>:
 
-        iter(&mut self) -> Iter<S, Self>;
+        iter(&self) -> Iter<S, Self>;
 
         append_node(&mut self, distance: S) -> Position<S, Self>;
         insert_node(&mut self, position: S) -> Position<S, Self>;
