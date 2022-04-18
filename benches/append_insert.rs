37f9f3f7d8996a1be0b8b2920fb2a680d76b40a3
AsterixxxGallier <asterixxxgallier@aol.com>
2022-04-18 20:21:03 +0200

    Slightly tweak append_insert benchmark and add more iterations to each iteration

--- benches/append_insert.rs
+++ benches/append_insert.rs
@@ -1,26 +1,26 @@
-use criterion::{black_box, Criterion, criterion_group, criterion_main};
+use criterion::{Criterion, criterion_group, criterion_main};
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
 use spaced_list_5::HollowSpacedList;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("append and insert");
-    group.bench_function("append 10k", |b| b.iter(|| {
+    group.bench_function("append", |b| b.iter(|| {
         let mut list = HollowSpacedList::new();
-        for _ in 0..10_000 {
+        for _ in 0..50_000 {
             list.append_node(1);
         }
     }));
-    group.bench_function("insert 10k", |b| b.iter(|| {
+    group.bench_function("insert", |b| b.iter(|| {
         let mut list = HollowSpacedList::new();
-        for n in 0..10_000 {
+        for n in 0..50_000 {
             list.insert_node(n);
         }
     }));
-    group.bench_function("insert random 10k", |b| b.iter(|| {
+    group.bench_function("insert random", |b| b.iter(|| {
         let mut list = HollowSpacedList::new();
         let mut rng = StdRng::seed_from_u64(0);
-        for _ in 0..10_000 {
+        for _ in 0..50_000 {
             list.insert_node(rng.gen_range(-1_000_000..1_000_000));
         }
     }));
