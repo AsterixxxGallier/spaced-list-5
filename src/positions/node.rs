5fc555fac71f705aa50b3cef0c1f6634752c85be
AsterixxxGallier <asterixxxgallier@aol.com>
2022-04-19 13:34:44 +0200

    Implement Clone for Position and add TODO comment to implement Copy too

--- src/positions/node.rs
+++ src/positions/node.rs
@@ -7,6 +7,19 @@ pub struct Position<'list, S: Spacing, List: SpacedList<S>> {
     position: S,
 }
 
+impl<'list, S: Spacing, List: SpacedList<S>> Clone for Position<'list, S, List> {
+    fn clone(&self) -> Self {
+        Self {
+            super_lists: self.super_lists.clone(),
+            list: self.list,
+            index: self.index,
+            position: self.position
+        }
+    }
+}
+
+// TODO implement Copy for Position by storing a reference to the super list along with the index in it
+
 impl<'list, S: Spacing, List: SpacedList<S>> Position<'list, S, List> {
     pub(crate) fn new(
         super_lists: Vec<&'list List>,
