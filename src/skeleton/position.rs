65e3b3707f2ccb1ff3ca094f87d34451e74fe865
AsterixxxGallier <asterixxxgallier@aol.com>
2022-04-23 21:35:59 +0200

    Put almost the entire implementation of both Position and HollowPosition in a single macro

--- src/skeleton/position.rs
+++ src/skeleton/position.rs
@@ -6,123 +6,179 @@ use maybe_owned::MaybeOwned;
 use crate::{ParentData, Spacing};
 use crate::skeleton::{Range, Skeleton};
 
-pub struct Position<Kind, S: Spacing, T> {
-    pub(crate) skeleton: Rc<RefCell<Skeleton<Kind, S, T>>>,
-    pub(crate) index: usize,
-    pub(crate) position: S,
+macro_rules! position {
+    ($name:ident; <Kind, S: Spacing$(, $T:ident)?>; $type:ty; $skeleton:ty) => {
+        pub struct $name<Kind, S: Spacing$(, $T)?> {
+            pub(crate) skeleton: Rc<RefCell<$skeleton>>,
+            pub(crate) index: usize,
+            pub(crate) position: S,
+        }
+
+        impl<Kind, S: Spacing$(, $T)?> Clone for $type {
+            fn clone(&self) -> Self {
+                Self {
+                    skeleton: self.skeleton.clone(),
+                    index: self.index,
+                    position: self.position,
+                }
+            }
+        }
+
+        impl<Kind, S: Spacing$(, $T)?> $type {
+            pub(crate) fn new(skeleton: Rc<RefCell<$skeleton>>, index: usize, position: S) -> Self {
+                Self {
+                    skeleton,
+                    index,
+                    position,
+                }
+            }
+
+            pub(crate) fn at_start(skeleton: Rc<RefCell<$skeleton>>) -> Self {
+                let position = skeleton.borrow().offset;
+                Self {
+                    skeleton,
+                    index: 0,
+                    position,
+                }
+            }
+
+            pub(crate) fn at_end(skeleton: Rc<RefCell<$skeleton>>) -> Self {
+                let index = skeleton.borrow().elements.len() - 1;
+                let position = skeleton.borrow().last_position();
+                Self {
+                    skeleton,
+                    index,
+                    position,
+                }
+            }
+
+            pub fn position(&self) -> S {
+                self.position
+            }
+
+            pub fn into_next(self) -> Option<Self> {
+                if self.index == self.skeleton.borrow().links.len() {
+                    if let Some(ParentData { parent, index_in_parent }) =
+                    &self.skeleton.clone().borrow().parent_data {
+                        let parent = parent.upgrade().unwrap();
+                        let position = self.position
+                            - self.skeleton.borrow().last_position()
+                            + parent.borrow().link(*index_in_parent);
+                        Some(Self {
+                            skeleton: parent,
+                            index: index_in_parent + 1,
+                            position,
+                        })
+                    } else {
+                        None
+                    }
+                } else if let Some(sub) =
+                self.skeleton.clone().borrow().sub(self.index) {
+                    let position = self.position + sub.borrow().offset;
+                    Some(Self {
+                        skeleton: sub,
+                        index: 0,
+                        position,
+                    })
+                } else {
+                    let position = self.position + self.skeleton.borrow().link(self.index);
+                    Some(Self {
+                        skeleton: self.skeleton,
+                        index: self.index + 1,
+                        position,
+                    })
+                }
+            }
+
+            pub fn into_previous(self) -> Option<Self> {
+                if self.index == 0 {
+                    if let Some(ParentData { parent, index_in_parent }) =
+                    &self.skeleton.clone().borrow().parent_data {
+                        let parent = parent.upgrade().unwrap();
+                        let position = self.position - self.skeleton.borrow().offset();
+                        Some(Self {
+                            skeleton: parent,
+                            index: *index_in_parent,
+                            position,
+                        })
+                    } else {
+                        None
+                    }
+                } else if let Some(sub) =
+                self.skeleton.clone().borrow().sub(self.index - 1) {
+                    let position = self.position
+                        - self.skeleton.borrow().link(self.index - 1)
+                        + sub.borrow().last_position();
+                    let index = sub.borrow().links.len();
+                    Some(Self {
+                        skeleton: sub,
+                        index,
+                        position,
+                    })
+                } else {
+                    let position = self.position - self.skeleton.borrow().link(self.index - 1);
+                    Some(Self {
+                        skeleton: self.skeleton,
+                        index: self.index - 1,
+                        position,
+                    })
+                }
+            }
+        }
+
+        impl<S: Spacing$(, $T)?> $name<Range, S$(, $T)?> {
+            pub fn bound_type(&self) -> BoundType {
+                BoundType::of(self.index)
+            }
+
+            pub fn span(&self) -> S {
+                self.skeleton.borrow().links[self.index & !1]
+            }
+
+            pub fn into_range(self) -> (Self, Self) {
+                match self.bound_type() {
+                    BoundType::Start => {
+                        let end = Self::new(
+                            self.skeleton.clone(),
+                            self.index + 1,
+                            self.position + self.span());
+                        (self, end)
+                    }
+                    BoundType::End => {
+                        let start = Self::new(
+                            self.skeleton.clone(),
+                            self.index - 1,
+                            self.position - self.span());
+                        (start, self)
+                    }
+                }
+            }
+
+            pub fn range(&self) -> (MaybeOwned<Self>, MaybeOwned<Self>) {
+                match self.bound_type() {
+                    BoundType::Start => {
+                        let end = Self::new(
+                            self.skeleton.clone(),
+                            self.index + 1,
+                            self.position + self.span());
+                        (self.into(), end.into())
+                    }
+                    BoundType::End => {
+                        let start = Self::new(
+                            self.skeleton.clone(),
+                            self.index - 1,
+                            self.position - self.span());
+                        (start.into(), self.into())
+                    }
+                }
+            }
+        }
+    };
 }
 
-impl<Kind, S: Spacing, T> Clone for Position<Kind, S, T> {
-    fn clone(&self) -> Self {
-        Self {
-            skeleton: self.skeleton.clone(),
-            index: self.index,
-            position: self.position,
-        }
-    }
-}
+position!(Position; <Kind, S: Spacing, T>; Position<Kind, S, T>; Skeleton<Kind, S, T>);
 
 impl<Kind, S: Spacing, T> Position<Kind, S, T> {
-    pub(crate) fn new(skeleton: Rc<RefCell<Skeleton<Kind, S, T>>>, index: usize, position: S) -> Self {
-        Self {
-            skeleton,
-            index,
-            position,
-        }
-    }
-
-    pub(crate) fn at_start(skeleton: Rc<RefCell<Skeleton<Kind, S, T>>>) -> Self {
-        let position = skeleton.borrow().offset;
-        Self {
-            skeleton,
-            index: 0,
-            position,
-        }
-    }
-
-    pub(crate) fn at_end(skeleton: Rc<RefCell<Skeleton<Kind, S, T>>>) -> Self {
-        let index = skeleton.borrow().elements.len() - 1;
-        let position = skeleton.borrow().last_position();
-        Self {
-            skeleton,
-            index,
-            position,
-        }
-    }
-
-    pub fn position(&self) -> S {
-        self.position
-    }
-
-    pub fn into_next(self) -> Option<Self> {
-        if self.index == self.skeleton.borrow().links.len() {
-            if let Some(ParentData { parent, index_in_parent }) =
-            &self.skeleton.clone().borrow().parent_data {
-                let parent = parent.upgrade().unwrap();
-                let position = self.position
-                    - self.skeleton.borrow().last_position()
-                    + parent.borrow().link(*index_in_parent);
-                Some(Position {
-                    skeleton: parent,
-                    index: index_in_parent + 1,
-                    position,
-                })
-            } else {
-                None
-            }
-        } else if let Some(sub) =
-        self.skeleton.clone().borrow().sub(self.index) {
-            let position = self.position + sub.borrow().offset;
-            Some(Position {
-                skeleton: sub,
-                index: 0,
-                position,
-            })
-        } else {
-            let position = self.position + self.skeleton.borrow().link(self.index);
-            Some(Position {
-                skeleton: self.skeleton,
-                index: self.index + 1,
-                position,
-            })
-        }
-    }
-
-    pub fn into_previous(self) -> Option<Self> {
-        if self.index == 0 {
-            if let Some(ParentData { parent, index_in_parent }) =
-            &self.skeleton.clone().borrow().parent_data {
-                let parent = parent.upgrade().unwrap();
-                let position = self.position - self.skeleton.borrow().offset();
-                Some(Position {
-                    skeleton: parent,
-                    index: *index_in_parent,
-                    position,
-                })
-            } else {
-                None
-            }
-        } else if let Some(sub) =
-        self.skeleton.clone().borrow().sub(self.index - 1) {
-            let position = self.position
-                - self.skeleton.borrow().link(self.index - 1)
-                + sub.borrow().last_position();
-            let index = sub.borrow().links.len();
-            Some(Position {
-                skeleton: sub,
-                index,
-                position,
-            })
-        } else {
-            let position = self.position - self.skeleton.borrow().link(self.index - 1);
-            Some(Position {
-                skeleton: self.skeleton,
-                index: self.index - 1,
-                position,
-            })
-        }
-    }
-
     pub fn element(&self) -> Ref<T> {
         Ref::map(RefCell::borrow(&self.skeleton),
                  |skeleton| &skeleton.elements[self.index])
@@ -134,54 +190,7 @@ impl<Kind, S: Spacing, T> Position<Kind, S, T> {
     }
 }
 
-pub struct HollowPosition<Kind, S: Spacing> {
-    pub(crate) skeleton: Rc<RefCell<Skeleton<Kind, S, ()>>>,
-    pub(crate) index: usize,
-    pub(crate) position: S,
-}
-
-impl<Kind, S: Spacing> Clone for HollowPosition<Kind, S> {
-    fn clone(&self) -> Self {
-        Self {
-            skeleton: self.skeleton.clone(),
-            index: self.index,
-            position: self.position,
-        }
-    }
-}
-
-impl<Kind, S: Spacing> HollowPosition<Kind, S> {
-    pub(crate) fn new(skeleton: Rc<RefCell<Skeleton<Kind, S, ()>>>, index: usize, position: S) -> Self {
-        Self {
-            skeleton,
-            index,
-            position,
-        }
-    }
-
-    pub(crate) fn at_start(skeleton: Rc<RefCell<Skeleton<Kind, S, ()>>>) -> Self {
-        let position = skeleton.borrow().offset;
-        Self {
-            skeleton,
-            index: 0,
-            position,
-        }
-    }
-
-    pub(crate) fn at_end(skeleton: Rc<RefCell<Skeleton<Kind, S, ()>>>) -> Self {
-        let index = skeleton.borrow().elements.len() - 1;
-        let position = skeleton.borrow().last_position();
-        Self {
-            skeleton,
-            index,
-            position,
-        }
-    }
-
-    pub fn position(&self) -> S {
-        self.position
-    }
-}
+position!(HollowPosition; <Kind, S: Spacing>; HollowPosition<Kind, S>; Skeleton<Kind, S, ()>);
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
 pub enum BoundType {
@@ -195,54 +204,6 @@ impl BoundType {
             0 => Self::Start,
             1 => Self::End,
             _ => unreachable!()
-        }
-    }
-}
-
-impl<S: Spacing> HollowPosition<Range, S> {
-    pub fn bound_type(&self) -> BoundType {
-        BoundType::of(self.index)
-    }
-
-    pub fn span(&self) -> S {
-        self.skeleton.borrow().links[self.index & !1]
-    }
-
-    pub fn into_range(self) -> (Self, Self) {
-        match self.bound_type() {
-            BoundType::Start => {
-                let end = HollowPosition::new(
-                    self.skeleton.clone(),
-                    self.index + 1,
-                    self.position + self.span());
-                (self, end)
-            }
-            BoundType::End => {
-                let start = HollowPosition::new(
-                    self.skeleton.clone(),
-                    self.index - 1,
-                    self.position - self.span());
-                (start, self)
-            }
-        }
-    }
-
-    pub fn range(&self) -> (MaybeOwned<Self>, MaybeOwned<Self>) {
-        match self.bound_type() {
-            BoundType::Start => {
-                let end = HollowPosition::new(
-                    self.skeleton.clone(),
-                    self.index + 1,
-                    self.position + self.span());
-                (self.into(), end.into())
-            }
-            BoundType::End => {
-                let start = HollowPosition::new(
-                    self.skeleton.clone(),
-                    self.index - 1,
-                    self.position - self.span());
-                (start.into(), self.into())
-            }
         }
     }
 }
