5c99ed0184cb0785afc9a535d7995dba7c98ef93
AsterixxxGallier <asterixxxgallier@aol.com>
2024-02-16 22:24:14 +0100

    Update stuff (nothing big)

--- benches/append_insert.rs
+++ benches/append_insert.rs
@@ -8,20 +8,20 @@ fn criterion_benchmark(c: &mut Criterion) {
     group.bench_function("append", |b| b.iter(|| {
         let mut list = HollowSpacedList::new();
         for _ in 0..50_000 {
-            list.append_node(1);
+            list.push(1);
         }
     }));
     group.bench_function("insert", |b| b.iter(|| {
         let mut list = HollowSpacedList::new();
         for n in 0..50_000 {
-            list.insert_node(n);
+            list.insert(n);
         }
     }));
     group.bench_function("insert random", |b| b.iter(|| {
         let mut list = HollowSpacedList::new();
         let mut rng = StdRng::seed_from_u64(0);
         for _ in 0..50_000 {
-            list.insert_node(rng.gen_range(-1_000_000..1_000_000));
+            list.insert(rng.gen_range(-1_000_000..1_000_000));
         }
     }));
     group.finish();
--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 //! 4. HollowRangeSpacedList: A spaced list that stores empty nodes as non-overlapping ranges.
 
 #![feature(trait_alias)]
-#![feature(generic_associated_types)]
+#![allow(unused)]
 
 use std::ops::{Add, AddAssign, Sub, SubAssign};
 
